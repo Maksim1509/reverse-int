dc110defe1af7122ecd0a4fa782ca075558de011
root <root@DESKTOP-DCAV93P.localdomain>
2022-01-23 20:10:09 +0400

    feat: implement reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+  const arr = n > 0 ? String(n).split('') : String(n).split('').slice(1);
+  const reversed = arr.reverse();
+  return Number(reversed.join(''));
 }
